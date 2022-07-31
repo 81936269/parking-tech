852953576240deb9bddd3b79a9e632f3ac82ccb8
Eduardo <eduardoaraujo56@hotmail.com>
2022-07-31 12:11:02 -0300

    Estrutura inicial

---
+++ src/main/java/estacionamento/parking/ParkingTechApplication.java
@@ -0,0 +1,14 @@
+package estacionamento.parking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingTechApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ParkingTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/estacionamento/parking/ParkingTechApplicationTests.java
@@ -0,0 +1,13 @@
+package estacionamento.parking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
